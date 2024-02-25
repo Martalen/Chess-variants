3c00132011079fce5539b9a36ae8b4e9d9030ad0
Martalen <114415190+Martalen@users.noreply.github.com>
2024-02-25 11:04:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/* document.getElementById("demo").innerHTML = "Hello World"
+   document.getElementById('myImage').src='hello world'
+   document.getElementById('demo').style.fontSize='35px'
+   document.getElementById('demo').style.display='none'
+   document.getElementById('demo').style.display='block'
+*/
